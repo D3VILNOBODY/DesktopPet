dfae1c4aa836aee5165e2d57a85c31f9419869b9
D3VILNOBODY <aaron.hughes0707@gmail.com>
2026-06-12 09:34:49 +0100

    Initial Commit

---
+++ DesktopPet/DesktopPetApp.swift
@@ -0,0 +1,16 @@
+//
+// DesktopPetApp.swift
+// Created by ferris on 12/06/2026.
+//
+    
+
+import SwiftUI
+
+@main
+struct DesktopPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
